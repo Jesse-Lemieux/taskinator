c463a42af0408a1d22427212228e9d7f4e17af47
JESSE <dtrsjesse121@gmail.com>
2022-02-09 18:43:52 -0500

    added prevent default

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
 // Variable for new task button
-var buttonEl = document.querySelector('#save-task');
+var formEl = document.querySelector("#task-form");
 // Variable for task list
 var tasksToDoEl = document.querySelector('#tasks-to-do');
 
 // Click function of button
 
 var createTaskHandler = function() {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
   }
 
-  buttonEl.addEventListener("click", createTaskHandler); 
+  formEl.addEventListener("submit", createTaskHandler); 
