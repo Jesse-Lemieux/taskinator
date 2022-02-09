3273caaad5607c562e39607a6dcc88abdb7ef7fa
JESSE <dtrsjesse121@gmail.com>
2022-02-09 18:54:52 -0500

    added form and dropdwon functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,25 @@
-// Variable for new task button
 var formEl = document.querySelector("#task-form");
-// Variable for task list
-var tasksToDoEl = document.querySelector('#tasks-to-do');
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-// Click function of button
+var createTaskHandler = function (event) {
+  event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-var createTaskHandler = function() {
+  // create list item
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
 
-    event.preventDefault();
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
 
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  }
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoEl);
 
-  formEl.addEventListener("submit", createTaskHandler); 
+  // add entire list item to list
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", createTaskHandler);
